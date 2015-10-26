8ae04903e386de93b4d00934c437a9e0148bdb95
Anton Klinskih <a.klinskih@carbonsoft.ru>
2015-10-26 17:31:20 +0500

    Добавлены коментарии

--- get_all_abonents.py
+++ get_all_abonents.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from api_class import Api
-
+#Зададим метод и парамерты для метода
 params = {
           'method1': 'objects.filter',
           'arg1': '{"is_folder":0}'
           }
 client=Api()
+#Вызовем апи для модели Abonents
 res_dict = client.call_api(model='Abonents',params=params)
 for row in res_dict['result']:
     print(row['fields']['name'])
